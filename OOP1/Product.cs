0c31239759269e2ca0b2a64beaa5f7c76d040150
rvzko <ravza.korkmaz@rub.de>
2021-01-20 20:30:41 +0100

    oop1

---
+++ OOP1/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    //Bu tip classlarda SADECE ÖZELLIK olur
+    class Product //Entity
+    {
+        public int Id { get; set; }
+        //Referans anahtar
+        public int CategoryId { get; set; } 
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+
+        //CRUD 
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2; // 2 = Mobilya
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product { Id = 2, CategoryId = 5, UnitsInStock = 5, ProductName = "Kalem", UnitPrice = 35 }; // 5 = Kirtasiye 
+
+            //PascalCase - ilk harf büyük   //camelCase - ilk harf kücük
+            //case sensitiv - kücük/büyük harf duyarli
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+
+            //int, double, bool ... deger tip
+            //diziler, class, abstract, interface ... referans tip
+
+            //ref out
+
+
+
+        }
+    }
+}
